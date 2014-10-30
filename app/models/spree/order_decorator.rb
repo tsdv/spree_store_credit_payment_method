1a44826dd22639e0ec2fcf0afd46591bc84a8b03
Peter Longyear <plongyear@bonobos.com>
2014-10-30 12:06:28 -0400

    Allow admins to redeem gift cards for users

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,5 @@
 module SpreeStoreCredits::OrderDecorator
-  def self.included(base)
-    base.state_machine.before_transition to: :confirm, do: :add_store_credit_payments
-    base.state_machine.after_transition to: :confirm, do: :create_gift_cards
+  extend ActiveSupport::Concern
 
   included do
     Spree::Order.state_machine.before_transition to: :confirm, do: :add_store_credit_payments
