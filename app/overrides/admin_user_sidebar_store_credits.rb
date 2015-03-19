aec72a3d08f55f9595fdd563b34586832fc6927a
Marc Leglise <mleglise@gmail.com>
2015-03-18 17:43:15 -0700

    Admin::Users: display store credit balance under Lifetime Stats.

--- app/overrides/admin_user_sidebar_store_credits.rb
+++ app/overrides/admin_user_sidebar_store_credits.rb
@@ -4,3 +4,10 @@ Deface::Override.new(
   insert_bottom: "[data-hook='admin_user_tab_options']",
   partial: "spree/admin/users/store_credits_sidebar",
 )
+
+Deface::Override.new(
+  virtual_path: "spree/admin/users/_sidebar",
+  name: "admin_user_sidebar_store_credit_balance",
+  insert_bottom: "dl#user-lifetime-stats",
+  partial: "spree/admin/users/store_credit_balance_sidebar",
+)
