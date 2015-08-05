d12f9328962b3fe78131a3c8a49e9ff0fcf83352
Marc Leglise <mleglise@gmail.com>
2015-08-05 14:58:10 -0700

    Admin::StoreCredits: copied views from spree/add-store-credits

--- app/overrides/admin_user_sidebar_store_credits.rb
+++ app/overrides/admin_user_sidebar_store_credits.rb
@@ -1,13 +1,15 @@
+# NOTE: Remove for 3-1-stable
+
 Deface::Override.new(
   virtual_path: "spree/admin/users/_sidebar",
   name: "admin_user_sidebar_store_credits",
   insert_bottom: "[data-hook='admin_user_tab_options']",
-  partial: "spree/admin/users/store_credits_sidebar",
+  partial: "spree/admin/users/store_credits_sidebar"
 )
 
 Deface::Override.new(
-  virtual_path: "spree/admin/users/_sidebar",
+  virtual_path: "spree/admin/users/_lifetime_stats",
   name: "admin_user_sidebar_store_credit_balance",
-  insert_bottom: "dl#user-lifetime-stats",
-  partial: "spree/admin/users/store_credit_balance_sidebar",
+  insert_bottom: "#user-lifetime-stats",
+  partial: "spree/admin/users/store_credit_balance_sidebar"
 )
