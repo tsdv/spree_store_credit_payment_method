26b6c84e3fcb48e4f8a8e99f24b77db10b291cd4
Marc Leglise <mleglise@gmail.com>
2015-08-05 11:26:22 -0700

    Separated Order::StoreCredit methods into a new module.
    Mimics the separation in spree/core

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,6 +6,8 @@ module SpreeStoreCredits::OrderDecorator
     base.has_many :gift_cards, through: :line_items
 
     base.prepend(InstanceMethods)
+
+    base.include Spree::Order::StoreCredit
   end
 
   module InstanceMethods
@@ -30,80 +32,6 @@ module SpreeStoreCredits::OrderDecorator
       end
     end
 
-    def add_store_credit_payments
-      payments.store_credits.where(state: 'checkout').map(&:invalidate!)
-
-      remaining_total = outstanding_balance
-
-      if user && user.store_credits.any?
-        payment_method = Spree::PaymentMethod.find_by_type('Spree::PaymentMethod::StoreCredit')
-        raise "Store credit payment method could not be found" unless payment_method
-
-        user.store_credits.order_by_priority.each do |credit|
-          break if remaining_total.zero?
-          next if credit.amount_remaining.zero?
-          next unless credit.valid?
-
-          amount_to_take = store_credit_amount(credit, remaining_total)
-          create_store_credit_payment(payment_method, credit, amount_to_take)
-          remaining_total -= amount_to_take
-        end
-      end
-
-      reconcile_with_credit_card(existing_credit_card_payment, remaining_total)
-
-      if payments.valid.sum(:amount) != total
-        errors.add(:base, Spree.t("store_credit.errors.unable_to_fund")) and return false
-      end
-    end
-
-    def covered_by_store_credit?
-      return false unless user
-      user.total_available_store_credit >= total
-    end
-    alias_method :covered_by_store_credit, :covered_by_store_credit?
-
-    def could_use_store_credit?
-      total_available_store_credit > 0
-    end
-
-    def total_available_store_credit
-      return 0.0 unless user
-      user.total_available_store_credit
-    end
-
-    def order_total_after_store_credit
-      total - total_applicable_store_credit
-    end
-
-    def using_store_credit?
-      total_applicable_store_credit > 0
-    end
-
-    def total_applicable_store_credit
-      if confirm? || complete?
-        payments.store_credits.valid.sum(:amount)
-      else
-        [total, (user.try(:total_available_store_credit) || 0.0)].min
-      end
-    end
-
-    def display_total_applicable_store_credit
-      Spree::Money.new(-total_applicable_store_credit, { currency: currency })
-    end
-
-    def display_order_total_after_store_credit
-      Spree::Money.new(order_total_after_store_credit, { currency: currency })
-    end
-
-    def display_total_available_store_credit
-      Spree::Money.new(total_available_store_credit, { currency: currency })
-    end
-
-    def display_store_credit_remaining_after_capture
-      Spree::Money.new(total_available_store_credit - total_applicable_store_credit, { currency: currency })
-    end
-
     private
 
     def after_cancel
@@ -122,52 +50,23 @@ module SpreeStoreCredits::OrderDecorator
       updater.persist_totals
     end
 
-    def existing_credit_card_payment
-      other_payments = payments.valid.not_store_credits
-      raise "Found #{other_payments.size} payments and only expected 1" if other_payments.size > 1
-      other_payments.first
-    end
-
-    def reconcile_with_credit_card(other_payment, amount)
-      return unless other_payment
-
-      unless other_payment.source.is_a?(Spree::CreditCard)
-        raise "Found unexpected payment method. Credit cards are the only other supported payment type"
-      end
-
-      if amount.zero?
-        other_payment.invalidate!
-      else
-        other_payment.update_attributes!(amount: amount)
-      end
-    end
-
-    def create_store_credit_payment(payment_method, credit, amount)
-      payments.create!(source: credit,
-                       payment_method: payment_method,
-                       amount: amount,
-                       state: 'checkout',
-                       response_code: credit.generate_authorization_code)
-    end
-
-    def store_credit_amount(credit, total)
-      [credit.amount_remaining, total].min
-    end
-
     # Override Spree::Core to use the remaining total
     def update_params_payment_source
       if @updating_params[:payment_source].present?
-        source_params = @updating_params.delete(:payment_source)[@updating_params[:order][:payments_attributes].first[:payment_method_id].to_s]
+        source_params = @updating_params.
+                        delete(:payment_source)[@updating_params[:order][:payments_attributes].
+                                                first[:payment_method_id].to_s]
 
         if source_params
           @updating_params[:order][:payments_attributes].first[:source_attributes] = source_params
         end
       end
 
-      if @updating_params[:order] && (@updating_params[:order][:payments_attributes] || @updating_params[:order][:existing_card])
+      if @updating_params[:order] && (@updating_params[:order][:payments_attributes] ||
+                                      @updating_params[:order][:existing_card])
         @updating_params[:order][:payments_attributes] ||= [{}]
-        # @updating_params[:order][:payments_attributes].first[:amount] = self.total
-        @updating_params[:order][:payments_attributes].first[:amount] = self.order_total_after_store_credit
+        # @updating_params[:order][:payments_attributes].first[:amount] = total
+        @updating_params[:order][:payments_attributes].first[:amount] = order_total_after_store_credit
       end
     end
 
