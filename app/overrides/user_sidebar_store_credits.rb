d20cf99b2058699d309550f7a772556d44994320
Marc Leglise <mleglise@gmail.com>
2015-08-05 16:51:28 -0700

    Checkout#summary: improved display of applied store credits and balance due.

--- app/overrides/user_sidebar_store_credits.rb
+++ app/overrides/user_sidebar_store_credits.rb
@@ -1,13 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/checkout/_summary',
   name: 'add_store_credit',
-  insert_before: '[data-hook="order_total"]',
+  insert_after: '[data-hook="order_total"]',
   partial: 'spree/checkout/store_credits_sidebar'
-)
-
-Deface::Override.new(
-  virtual_path: 'spree/checkout/_summary',
-  name: 'replace_order_total',
-  replace_contents: '[data-hook="order_total"]',
-  partial: 'spree/checkout/balance_due_sidebar',
 )
