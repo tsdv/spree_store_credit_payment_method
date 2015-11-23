2df568ac3e2d2745215559894aa3b5594f58d585
Novik <novik65@gmail.com>
2015-11-23 20:31:01 +0300

    minor fix

--- app/models/spree/reimbursement_type/original_payment.rb
+++ app/models/spree/reimbursement_type/original_payment.rb
@@ -3,9 +3,8 @@ class Spree::ReimbursementType::OriginalPayment < Spree::ReimbursementType
 
   class << self
     def reimburse(reimbursement, return_items, simulate)
-      unpaid_amount = return_items.sum(&:total).round(2, :down)
+      unpaid_amount = return_items.sum(&:total).to_d.round(2, :down)
       payments = reimbursement.order.payments.completed
-
       refund_list, unpaid_amount = create_refunds(reimbursement, payments, unpaid_amount, simulate)
       if unpaid_amount > 0.0
         refund_list, unpaid_amount = self.create_credits(reimbursement, unpaid_amount, simulate, refund_list)
